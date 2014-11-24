b9d90384623cf471e2bfcc77c3c54a043818894f
dankernel <google@dankook.ac.kr>
2014-11-25 03:02:22 +0900

    auto-20141125-030222

---
+++ code/noroot/compile.sh
@@ -0,0 +1,2 @@
+gcc -finput-charset=UTF-8 -o main main.c
+./main
---
+++ code/noroot/git_backup.sh
@@ -0,0 +1,9 @@
+git add .
+
+MSG=auto'-'`date +%Y%m%d'-'%H%M%S`
+git commit -m "$MSG"
+
+git push
+
+work="[OK] ".$(pwd)
+echo -e '\E[34m'"\033[1m$work\033[0m"
