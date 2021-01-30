aaf4aa7c6463927e7efa6e5c7ffb59dbb5415078
dankernel <dkdkernel@gmail.com>
2021-01-31 01:19:56 +0900

    remove config dir

--- .config/chromium/Default/Extensions/hclgegipaehbigmbhdpfapmjadbaldib/0.2.6.2_0/js/injection.js
+++
@@ -1,7 +0,0 @@
-/*!
- * JavaScript for Injection
- *
- * @author Tetsuwo OISHI
- */
-
-chrome.extension.sendRequest({ title: document.title, url: location });
--- .config/chromium/Default/Extensions/hompjdfbfmmmgflfjdlnkohcplmboaeo/1.3.5_0/js/thank_you.js
+++
@@ -1,9 +0,0 @@
-
-var APP_ID = 267128753314780;
-var URL_TO_LIKE = "https://www.facebook.com/pages/Allow-Right-Click-Chrome-Extension/317208101708296";
-
-var parent = document.getElementById("fb-like");
-var src = "http://www.facebook.com/plugins/like.php?href=" + encodeURIComponent(URL_TO_LIKE) + 
-          "&amp;send=false&amp;layout=box_count&amp;width=60&amp;show_faces=false&amp;font&amp;" + 
-          "colorscheme=light&amp;action=like&amp;height=90&amp;appId=" + APP_ID;
-parent.innerHTML = '<iframe src="'+ src +'" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:60px; height:65px;" allowTransparency="true"><\/iframe>';
